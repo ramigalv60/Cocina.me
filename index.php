95f60e0d029647bedcd2179daf88f1c4c13e6162
TomasSegui <127337211+TomasSegui@users.noreply.github.com>
2023-08-11 10:16:37 -0300

    Navbar
    hice la navbar y empecé con la seccion de recetas

--- index.php
+++ index.php
@@ -1,10 +1,38 @@
 <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cocina.Me</title>
+    <link rel="stylesheet" href="style.css"
+</head>
 
-<html>
-    <head>
-        <title>Cocina.Me</title>
-    </head>
-   <body>
-       
-   </body> 
+<body>
+<!---HEADER-->
+<header>
+<nav>
+  <label class="logo">Cocina.Me</label>
+  <ul>
+      <li><a class="active" href="#">Inicio</a></li>
+      <li><a href="https://www.cedicas.com.ar/">Cedicas</a></li>
+      <li><a href="#">Recetas</a></li>
+      <li><a href="#">Contactos</a></li>
+  </ul>
+</nav>
+
+</header>
+<!--MAIN-->
+<main>
+<img class="comedor" src="Imagenes/comerdor.jpg">
+<img  src="Imagenes/Reposteria.jpg"
+
+</main>
+<!--FOOTER-->
+<footer>
+
+
+</footer>
+</body>
+
 </html>
